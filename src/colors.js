273e8b2f29d03b6d442a3eb73978b4e34bf69349
Valentin Barit <valentin.barit@gmail.com>
2022-01-26 16:29:06 +0100

    feat: 🎸 add colors
    COSMIC, BONE, RED, ORANGE, and GREEN

--- src/colors.js
+++ src/colors.js
@@ -1,6 +1,11 @@
 const colors = {
   RAINBOW:
     "linear-gradient(90deg, #FB84C6, #E3E2C3 24%, #A8FFC3 49%, #98DDFF 76%, #C587FF)",
+  COSMIC: "#333333",
+  BONE: "#fffdf6",
+  RED: "#ff8b92",
+  ORANGE: "#ffdca8",
+  GREEN: "#98e4ad",
 };
 
 export default colors;
