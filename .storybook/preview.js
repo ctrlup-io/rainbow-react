9ee1081c4b5f808fd0ce0805d5b1ce08f45ea1de
Valentin Barit <quibaritaenperdresatrompe@users.noreply.github.com>
2022-02-22 13:32:38 +0100

    feat: 🎸 provide MUI theme (#3)
    * feat: 🎸 use MUI
    
    * ci: 🎡 add release job
    
    * docs: ✏️ add storybook link
    
    * docs: ✏️ add colors to story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,45 @@
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { ThemeProvider as EmotionThemeProvider } from "emotion-theming";
+import { Box } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+import theme from "../src/theme";
+
+const muiTheme = createTheme(theme);
+
+export const decorators = [
+  (Story) => (
+    <EmotionThemeProvider theme={muiTheme}>
+      <MuiThemeProvider theme={muiTheme}>{Story()}</MuiThemeProvider>
+    </EmotionThemeProvider>
+  ),
+  (Story) => (
+    <Box
+      sx={{
+        background: muiTheme.palette.background.default,
+        padding: muiTheme.spacing(2),
+      }}
+    >
+      {Story()}
+    </Box>
+  ),
+];
+
 export const parameters = {
+  options: {
+    storySort: {
+      order: ["Colors", "Fonts", "Theme"],
+    },
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  backgrounds: {
+    default: "dark",
+    values: [{ name: "dark", value: muiTheme.palette.background.default }],
   },
 };
--- README.md
+++ README.md
@@ -22,7 +22,7 @@ This package provides you with a library of [React](https://reactjs.org/) compon
 <table>
   <tr>
     <th>@rainbow/react</th>
-    <td><a href=""><img src="https://img.shields.io/badge/storybook-react-61DAFB?style=flat&logo=storybook" alt="storybook" /></a></a></td>
+    <td><a href="https://rainbow-react.netlify.app/"><img src="https://img.shields.io/badge/storybook-react-61DAFB?style=flat&logo=storybook" alt="storybook" /></a></a></td>
     <td><a href=""><img src="https://img.shields.io/npm/v/@rainbow/react?style=flat&logo=npm" alt="npm version" /></a></td>
   </tr>
 </table>
