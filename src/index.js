1d068089ec70324d3fd4542c81f6a8a892a09d61
Valentin Barit <valentin.barit@gmail.com>
2022-03-25 16:39:05 +0100

    fix: 🐛 expose Rainbow module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-export { default as colors } from "./colors/index.js";
-export { default as theme } from "./theme";
 export { default as AboutIcon } from "./icons/About";
 export { default as BlogIcon } from "./icons/Blog";
+export { default as Card } from "./Card";
+export { default as CollapseCard } from "./CollapseCard";
+export { default as colors } from "./colors/index.js";
 export { default as CommunityIcon } from "./icons/Community";
 export { default as ContactIcon } from "./icons/Contact";
 export { default as HomeIcon } from "./icons/Home";
 export { default as JobIcon } from "./icons/Job";
+export { default as Rainbow } from "./Rainbow";
 export { default as ServiceIcon } from "./icons/Service";
 export { default as TeamIcon } from "./icons/Team";
-export { default as CollapseCard } from "./CollapseCard";
-export { default as Card } from "./Card";
+export { default as theme } from "./theme";
